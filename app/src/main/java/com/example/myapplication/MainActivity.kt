5e26ee9b3413920370694f8f8588f5dc87c20b75
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2025-04-05 11:25:25 +0530

    fixing bottom nav bug

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,20 +2,17 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.ui.gallery.GalleryFragment
-import com.example.myapplication.ui.home.HomeFragment
-import com.example.myapplication.ui.slideshow.SlideshowFragment
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,35 +35,21 @@ class MainActivity : AppCompatActivity() {
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val bottomNavView: BottomNavigationView =findViewById(R.id.bottom_navigation);
+        val bottomNavView = binding.root.findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
+        // Set up AppBarConfiguration with both DrawerLayout & Bottom Navigation items
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_fav, R.id.nav_trends, R.id.nav_cart), drawerLayout
+            setOf(
+                R.id.nav_home,
+                R.id.nav_gallery,
+                R.id.nav_slideshow
+            ), drawerLayout
         )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         bottomNavView.setupWithNavController(navController)
-
-        bottomNavView.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_fav -> {
-                    showHomeFragment()
-                    true
-                }
-                R.id.nav_cart-> {
-                    showSavedListFragment()
-                    true
-                }
-                R.id.nav_trends -> {
-                    showChatsListFragment()
-                    true
-                }
-               
-                else -> false
-            }
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -78,30 +61,4 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-    private fun showHomeFragment() {
-
-        val homeFragment = GalleryFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment_content_main, homeFragment)
-            .commit()
-    }
-
-    private fun showChatsListFragment() {
-      
-        val chatsListFragment =HomeFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment_content_main, chatsListFragment)
-            .commit()
-    }
-
-    private fun showSavedListFragment() {
-       
-        val savedListFragment = SlideshowFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.nav_host_fragment_content_main, savedListFragment)
-            .commit()
-    }
-
-  
 }
