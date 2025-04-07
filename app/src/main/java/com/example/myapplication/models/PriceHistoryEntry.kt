ab702f89f57bc0d75703426eb9c80858c6e7fa23
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2025-04-07 11:47:58 +0530

    Undo API due to bug

--- app/src/main/java/com/example/myapplication/models/PriceHistoryEntry.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.models
-
-data class PriceHistoryEntry(
-    val date: Long, // Unix timestamp
-    val price: Float,
-    val source: String // e.g. "Amazon", "Flipkart"
-)
